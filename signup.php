bbdfd27feee53059dc051cc24dc73116006d74c2
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-03-26 18:25:40 +0530

    Added Signup Form HTML

---
+++ signup.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up | Expense Tracker</title>
+</head>
+<body>
+<div class="container">
+    <form action="" method="post">
+    <h1>Name : </h1>
+    <input type="text" name="nm" placeholder="Enter Your Name" require>
+    <h1>Email ID : </h1>
+    <input type="email" name="email"  placeholder="Enter Your Email" require>
+    <h1>Mobile Number : </h1>
+    <input type="number" name="mobile" placeholder="Enter Your Mobile Number" require>
+    <h1>Password : </h1>
+    <input type="password" name="passwd" id="password1" placeholder="Enter Your Password" require>
+    <h1>Confirm Password : </h1>
+    <input type="password" name="passwd_confirm" id="password2" placeholder="Enter Your Password" require>
+    <p id="message"></p>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+
+</div>
+<script>
+        const password1 = document.getElementById('password1');
+        const password2 = document.getElementById('password2');
+        const message = document.getElementById('message');
+
+        
+        function checkPasswordMatch() {
+            if (password1.value === password2.value) {
+                message.textContent = 'Passwords match';
+                message.style.color = 'green';
+            } else {
+                message.textContent = 'Passwords do not match';
+                message.style.color = 'red';
+            }
+        }
+
+      
+        password1.addEventListener('input', checkPasswordMatch);
+        password2.addEventListener('input', checkPasswordMatch);
+    </script>
+</body>
+</html>
+<?php
+
+
+
+?>
