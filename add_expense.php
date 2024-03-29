c41998c3bef3e6d696f47e8c47bba3c76f1f529e
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-03-29 10:16:50 +0530

    Added Add Expense Method code

--- add_expense.php
+++ add_expense.php
@@ -1,3 +1,11 @@
 <?php
-
+if($_SERVER['REQUEST_METHOD']=='GET')
+{
+    $name=$_GET['name'];
+    $cat=$_GET['category'];
+    $desc=$_GET['description'];
+    $time=date('%D-%M-%Y');
+    $amount-$_GET['amount'];
+    
+}
 ?>
