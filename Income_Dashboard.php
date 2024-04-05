5f02a3862df2daa3df1f5b8826a0a6d55fa42afd
VedantTuvar <vedanttuvar01@gmail.com>
2024-04-05 20:13:38 +0530

    Updated

---
+++ Income_Dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Income Dashboard</title>
+    <link rel="stylesheet" href="./static/Income_Dashboard.css">
+</head>
+<body>
+    <nav>
+    <input type="checkbox" id="check" />
+    <label for="check" class="checkbtn">
+      <img class="menu" src="bars.svg" alt="Bars" />
+    </label>
+    <img src="/Images/Budget Buddy Logo.png" alt="">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Feedback</a></li>
+    </ul>
+  </nav>
+</body>
+</html>
